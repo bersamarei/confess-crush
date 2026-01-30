b942528aa9f684d066e452318149bb0a954a724f
Alamak <mizunarei60@gmail.com>
2026-01-30 15:50:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    titleWeb: "Confess Crush",
+    introTitle: 'Hai maniesss!',
+    introDesc: `jadi gini aku ga tau nih mau ngapain buat nembah jadi ku bikin ini aja deh yaa yang simpel ajahh hehe`,
+    btnIntro: 'Dencet Dong',
+    title: 'mau jadi pacar aku ga maniess 🥰',
+    desc: 'ku tantang bilang enggak kalo bisa ',
+    btnYes: 'Okii mauu <33',
+    btnNo: 'Ga mau ahh',
+    question: 'Apa alasanya mau jadi pacarku <3',
+    btnReply: 'pencet dong <3',
+    reply: 'Karena aku sayang sama kamu <33333333',
+    mess: 'yeyyy sekarang kita jadian 🥰😘😘',
+    messDesc: 'walaupun aku ga peka moga betah yahh.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
+a
